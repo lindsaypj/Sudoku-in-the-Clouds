a020d289d6be244f8874fea8bcdf69eafdd6f98f
Patrick Lindsay <lindsay.patrick@student.greenriver.edu>
2023-02-08 08:27:17 -0800

    Added GameMode toggleability

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import GameMenu from './components/GameMenu';
 import Testing from './views/Testing';
+import SudokuCasual from './views/SudokuCasual';
+import SudokuHome from './views/SudokuHome';
 
 import './styles/css/App.css';
+
 
 // Define game modes
 const GameModes = {
@@ -13,13 +16,29 @@ const GameModes = {
 }
 
 function App() {
-  // Hooks to manage Game State
-  const [gameMode, setGameMode] = useState(GameModes.Testing);
+  // Hooks to manage App State
+  const [gameMode, setGameMode] = useState(GameModes.Testing);    // Game Mode
+  const [menuVisibility, setMenuVisibility] = useState(false);    // Game Menu visibility
+
+  function renderGameMode(gameMode) {
+    switch(gameMode) {
+      case GameModes.Testing:
+        return <Testing />;
+      case GameModes.Sudoku4x4Casual:
+        return <SudokuCasual size={4} />;
+      case GameModes.Sudoku9x9Casual:
+        return <SudokuCasual size={9} />;
+      case GameModes.Sudoku16x16Casual:
+        return <SudokuCasual size={16} />;
+      default:
+        return <SudokuHome />;
+    }
+  }
 
   return (
     <div className="App bg-light">
       <GameMenu setGameMode={setGameMode} />
-      <Testing />
+      {renderGameMode(gameMode)}
     </div>
   );
 }
--- src/components/GameMenu.jsx
+++ src/components/GameMenu.jsx
@@ -15,13 +15,6 @@ function GameMenu(props) {
 
     return (
         <>
-        {/* Menu button */}
-        <Button variant="primary" onClick={handleShow}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-grid-3x3" viewBox="0 0 16 16">
-                <path d="M0 1.5A1.5 1.5 0 0 1 1.5 0h13A1.5 1.5 0 0 1 16 1.5v13a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13zM1.5 1a.5.5 0 0 0-.5.5V5h4V1H1.5zM5 6H1v4h4V6zm1 4h4V6H6v4zm-1 1H1v3.5a.5.5 0 0 0 .5.5H5v-4zm1 0v4h4v-4H6zm5 0v4h3.5a.5.5 0 0 0 .5-.5V11h-4zm0-1h4V6h-4v4zm0-5h4V1.5a.5.5 0 0 0-.5-.5H11v4zm-1 0V1H6v4h4z"/>
-            </svg>
-        </Button>
-
         {/* Game Menu Modal */}
         <Modal 
             show={show} 
@@ -45,9 +38,6 @@ function GameMenu(props) {
             <Modal.Footer className='justify-content-center'>
             <Button variant="secondary" onClick={handleClose}>
                 Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-                Save Changes
             </Button>
             </Modal.Footer>
         </Modal>
