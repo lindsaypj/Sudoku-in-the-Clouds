43081d664e5038482bf431d80dd3db609b2f745a
Patrick Lindsay <lindsay.patrick@student.greenriver.edu>
2023-02-13 16:12:59 -0800

    Added fetch function for getting a sudoku board

--- src/views/SudokuCasual.jsx
+++ src/views/SudokuCasual.jsx
@@ -2,11 +2,43 @@ import React from "react";
 import { useState } from "react";
 import SudokuBoard from "../components/boards/SudokuBoard";
 
-function SudokuCasual() {
-    // Hooks to manage Game state
+function SudokuCasual(props) {
+
+    // Function to get a random board from SudokuAPI
+    // Use GET @ http://localhost:8080/sudoku/boards/random
+    function GETRandomBoard() {
+        const uri = "http://localhost:8080/sudoku/board/"+props.size+"x"+props.size;
+        console.log(uri);
+        const params = {
+            method: "get",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        // Send fetch request and wait for response
+        fetch(uri, params)
+            .then(function(response) {
+                if (response.ok) {
+                    return response.json();
+                }
+                else {
+                    console.log("ERROR: "+response);
+                }
+            })
+            .then(function(board) {
+                // Board should be int array
+                console.log(board);
+            });
+    }
+
     return (
-        <>
-        </>
+        <div className="container-fluid">
+            <div className="row">
+                <SudokuBoard size={props.boardSize} initialData={GETRandomBoard()} />
+            </div>
+        </div>
     );
 }
 
