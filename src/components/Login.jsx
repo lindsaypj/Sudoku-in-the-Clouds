1ebc2526f1bdf344934d9bc20b22327f58bd7d5a
Patrick Lindsay <lindsay.patrick@student.greenriver.edu>
2023-02-18 10:11:08 -0800

    Feat: Added login fetch GET request

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login(props) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [errorMessage, setErrorMessage] = useState("");
+
+    // Attempt to login
+    function loginAttempt(event) {
+        // Prevent reload
+        event.preventDefault();
+
+        const loginURI = "http://localhost:8080/sudoku/users/"+username+"?token="+password;
+        const params = {
+            method: "get",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        fetch(loginURI, params)
+        .then(function (response) {
+            if (response.ok) {
+                setErrorMessage("");
+                return response.json();
+            }
+            else {
+                if (response.status === 400) {
+                    setErrorMessage("Username or password is invalid");
+                }
+                console.log("ERROR: "+response.status+ " " + response.url);
+            }
+        })
+        .then(function (user) {
+            props.setUser(user);
+            props.setNewUser(false);
+            sessionStorage.setItem('user', JSON.stringify(user));
+        });
+    }
+
     return (
         <div className="bg-light text-center mt-5 container-fluid">
             <div className="row">
@@ -8,15 +47,27 @@ export default function Login(props) {
                 <div className="col-12 col-md-1"></div>
                 <div className="col-12 col-md-6">
                     <h2 className="mb-4">Please Log In</h2>
-                    <form>
+                    <form onSubmit={(e) => {loginAttempt(e)}}>
                         <div className="d-block m-2">
                             <label>
-                                <input type={"text"} className="form-control border-0" placeholder="Username" />
+                                <input 
+                                    type={"text"}
+                                    className="form-control border-0"
+                                    value={username}
+                                    onChange={(e) => {setUsername(e.target.value)}}
+                                    placeholder="Username" 
+                                />
                             </label>
                         </div>
                         <div className="d-block m-2">
                             <label>
-                                <input type={"password"} className="form-control border-0" placeholder="Password" />
+                                <input
+                                    type={"password"}
+                                    className="form-control border-0"
+                                    value={password}
+                                    onChange={(e) => {setPassword(e.target.value)}}
+                                    placeholder="Password" 
+                                />
                             </label>
                         </div>
                         
