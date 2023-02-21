964d0c8a8e9ce97eefe2b26eee62d252d21a17e8
Patrick Lindsay <lindsay.patrick@student.greenriver.edu>
2023-02-20 18:01:43 -0800

    Feat: Board state is preserved in session object

--- src/components/boards/Cell.jsx
+++ src/components/boards/Cell.jsx
@@ -9,38 +9,38 @@ const COLORS = ["black", "blue", "red", "green", "yellow", "purple", "orange", "
 const INPUT_PATTERNS = {4:"[0-4]", 9:"[0-9]", 16:"[0-9]|1[0-6]"};
 
 // Component Function
-function Cell(props) {
+function Cell({ cellIndex, size, value, cellUpdateCallback, boardIndex, textVisibility, disabled }) {
     // Hooks to manage cell value
     const [inputValue, setInputValue] = useState("");
-    const [displayValue, setDisplayValue] = useState(props.value);
+    const [displayValue, setDisplayValue] = useState(value);
 
     // Hook to manage input pattern (prevents unwated input values)
-    const [inputPattern, setInputPattern] = useState(INPUT_PATTERNS[props.size]);
+    const [inputPattern, setInputPattern] = useState(INPUT_PATTERNS[size]);
     
     // Hooks to manage cell color
     // Background Color
     const [cellBGColor, setCellBGColor] = useReducer(() => {
-        if (props.textVisibility) {
+        if (textVisibility) {
             return "transparent";
         }
         return COLORS[displayValue];
-    }, props.value);
+    }, value);
     // Text Color
     const [cellTextColor, setCellTextColor] = useReducer(() => {
         if (displayValue === 0 || displayValue === "0") {
             return "transparent";
         }
-        else if (props.textVisibility) {
+        else if (textVisibility) {
             return COLORS[0];
         }
         return COLORS[displayValue];
-    }, props.value);
+    }, value);
     
 
     // Render display value on input update
     function handleNewInputValue(newValue) {
         // Clear input value unless expecting second digit
-        if (props.size > 9) {
+        if (size > 9) {
             setInputValue(newValue);
         }
         else {
@@ -54,6 +54,8 @@ function Cell(props) {
         else {
             setDisplayValue(newValue);
         }
+        // Update board state
+        cellUpdateCallback(cellIndex, Number.parseInt(newValue));
 
         // Set cell colors
         setCellBGColor();
@@ -64,34 +66,27 @@ function Cell(props) {
     useEffect(() => {
         setCellBGColor();
         setCellTextColor();
-    }, [props.textVisibility]);
+    }, [textVisibility]);
 
     // Set Input pattern on size change
     useEffect(() => {
-        setInputPattern(INPUT_PATTERNS[props.size]);
-    },[props.size]);
+        setInputPattern(INPUT_PATTERNS[size]);
+    },[size]);
 
 
     return (
         <div className='cellContainer'>
-            <p className={"cellDisplay size-"+props.size + " cell-text-"+cellTextColor}>{displayValue}</p>
+            <p className={"cellDisplay size-"+size + " cell-text-"+cellTextColor}>{displayValue}</p>
             {/* Cell input element (Interactable) */}
             <input 
                 type={'text'}
-                className={"cell size-"+props.size+" cell-bg-"+cellBGColor}
-                tabIndex={props.index+1}
+                className={"cell size-"+size+" cell-bg-"+cellBGColor}
+                tabIndex={boardIndex}
                 pattern={inputPattern}
-                value={inputValue} 
+                value={inputValue}
+                disabled={disabled}
                 onChange={(e) =>
-                    handleNewInputValue(e.target.validity.valid ? e.target.value : "")}
-            />
-            {/* Cell return value (Hidden) */}
-            <input 
-                type={"hidden"}
-                className={"cell-return"}
-                aria-hidden={true}
-                name={"cells["+(props.index+1)+"]"}
-                value={displayValue}
+                    handleNewInputValue(e.target.validity.valid ? e.target.value : 0)}
             />
         </div>
     );
--- src/components/boards/SudokuBoard.jsx
+++ src/components/boards/SudokuBoard.jsx
@@ -3,56 +3,94 @@ import Cell from "./Cell.jsx";
 
 import '../.././styles/css/board.css';
 
-function SudokuBoard(props) {
-    // Hooks to Manage Board state
-    const [boardSize, setBoardSize] = useState(props.size);
-    const [tabIndexOffset, setBoardIndex] = useState(props.boardIndex * boardSize * boardSize);
+function SudokuBoard({ size, initialBoard, boardIndex, hideNums }) {
 
-    // Update Board when parameters change
-    useEffect(() => {
-        setBoardSize(props.size);
-        setBoardIndex(props.boardIndex * props.size * props.size);
-    }, [props]);
+    ////    BOARD STATE MANAGMENT    ////
 
-    // Get cell data if it exists, or create blank board
-    let cells = [];
-    if (props.initialBoard) {
-        cells = props.initialBoard;
+    const [cells, setCells] = useState([]);
+    const boardIdentifier = size+"-board-"+boardIndex;
+    const cellCount = size*size;
+
+    // Callback function to update the board when a cell is updated
+    function updateCellOnBoard(index, value) {
+        let valid = true;
+        // Validate index
+        if (isNaN(index) || index < 0 || index > cells.length) {
+            valid = false;
+        }
+        // Validate value
+        if (isNaN(value) || value < 0 || value > size) {
+            valid = false;
+        }
+
+        // Update BoardState
+        if (valid) {
+            cells[index] = value;
+            sessionStorage.setItem(boardIdentifier, JSON.stringify(cells));
+        }
     }
+
+
+    ////    BOARD INITIALIZATION    ////
+
+    // Load board data from session on load
+    // Check if session data exists
+    const loadCells = JSON.parse(sessionStorage.getItem(boardIdentifier));
+    if (loadCells !== null && loadCells !== undefined) {
+        for (let i = 0; i < loadCells.length; i++) {
+            cells[i] = loadCells[i];
+        }
+    }
+    // Check if initialBoard was passed
+    else if (initialBoard !== undefined && initialBoard !== null && initialBoard.length === cellCount) {
+        cells.push(...initialBoard);
+    }
+    // Load blank board
     else {
-        // Initialze board with no data
-        for (let i = 0; i < boardSize * boardSize; i++) {
+        for (let i = 0; i < cellCount; i++) {
             cells[i] = 0;
         }
     }
-    
 
-    const cellRows = new Array(cells.length);
 
-    // Loop over cells to assign to row/group
-    for (let cellRow = 0; cellRow < boardSize; cellRow++) {
-        const newRow = [];
-        for (let col = 0; col < boardSize; col++) {
-            newRow[col] = cells[col + (cellRow * boardSize)];
+    ////    RENDER BOARD    ////
+
+    // Generate Rows and cols to map through
+    const cellRows = [];
+    for (let row = 0; row < size; row++) {
+        const cellCols = [];
+        for (let col = 0; col < size; col++) {
+            cellCols[col] = row * size + col;
         }
-        cellRows.push(newRow);
+        cellRows[row] = cellCols;
     }
-    
+
     return(
-        <div className={"board board-"+ boardSize}>
+        <div className={"board board-"+ size}>
             {/* Map over rows */}
             {cellRows.map((row, rowIndex) => {
                 return (
                 <div key={rowIndex} className="cell-row">
-                    {row.map((cellValue, cellIndex) => {
+                    {row.map((cellValue, colIndex) => {
+                        // Store value in BoardState
+                        
+                        // Prevent editing initial values
+                        let disabled = true;
+                        if (initialBoard === undefined || initialBoard[colIndex] === 0) {
+                            disabled = false;
+                        }
+                        const cellIndex = (rowIndex * size) + colIndex;
                         return (
-                        <Cell 
-                            key={cellIndex}
-                            value={cellValue} 
-                            size={boardSize} 
-                            textVisibility={!(props.hideNums)}
-                            index={tabIndexOffset + (rowIndex*boardSize) + cellIndex} 
-                        />
+                            <Cell
+                                key={cellIndex}
+                                cellIndex={cellIndex}
+                                size={size}
+                                value={cells[cellIndex]} 
+                                cellUpdateCallback={updateCellOnBoard}
+                                textVisibility={!(hideNums)}
+                                boardIndex={boardIndex + cellIndex}
+                                disabled={disabled}
+                            />
                         );
                     })}
                 </div>
